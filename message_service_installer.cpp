c4f008ada73b32c91b022ee887353012b728aadc
hamin Song <stdhamin@gmail.com>
2024-05-27 17:38:56 +0900

    네트워크 중간단계1

--- message_service_installer.cpp
+++ message_service_installer.cpp
@@ -3,7 +3,7 @@
 // 호스트에 MessageService를 설치한다
 MessageService *MessageServiceInstaller::install(Host *host)
 {
-    MessageService* m = new MessageService(host, host->address().toShort(), this->destAddress_, this->destPort_);
+    m = new MessageService(host, host->address().toShort(), this->destAddress_, this->destPort_);
     ServiceInstaller::install(host, m);
     return m;
 }
